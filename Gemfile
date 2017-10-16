0bb7b779f2f64abe0978f12f9b7e4b7f90573885
一宮百合子 <ichinomiyayuriko@mba.local>
2017-10-16 14:50:05 +0900

    heroku_secret_token

--- Gemfile
+++ Gemfile
@@ -51,3 +51,5 @@ gem 'twitter-bootstrap-rails'
 
 gem 'devise'
 
+gem 'rails_12factor', group: :production
+
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
   end
 
   root 'top#index'
+
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
