f024227dcb9c80411d51e201e0e79665942595c9
yuriko <yuri715lily_momo@yahoo.co.jp>
2017-11-08 12:39:38 +0900

    画像pathの修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,20 +6,19 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
- config.action_mailer.default_url_options = { host: 'localhost:3000' }
- config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener_web
 
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener_web
 
 
